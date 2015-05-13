002a3e2fdcd47f141d1d1725fedd2393c8b31794
Alessio Rocco <alessio.rocco.lt@gmail.com>
2015-05-13 15:04:37 +0200

    Change and fix ItemAdjustments to the new AdjustmentsUpdater

---
+++ app/models/spree/adjustable/adjustments_updater_decorator.rb
@@ -0,0 +1,20 @@
+Spree::Adjustable::AdjustmentsUpdater.class_eval do
+
+  module OverrideAdjustmentsUpdater
+    def update
+      super
+
+      handling_total = 0
+      handling_total = adjustments.handling.reload.map(&:update!).compact.sum
+
+      adjustable.update_columns(
+        handling_total: handling_total,
+        adjustment_total: adjustable.adjustment_total + handling_total,
+        updated_at: Time.now
+      )
+    end
+  end
+
+  prepend OverrideAdjustmentsUpdater
+
+end
